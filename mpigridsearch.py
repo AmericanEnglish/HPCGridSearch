5a9b66579aca8f3b27cbded435f0771d9ff4ce16
Carlos Barajas <barajasc@umbc.edu>
2019-10-25 03:58:07 -0400

    codetools and mpi_logic have been moved into the search object

---
+++ mpigridsearch.py
@@ -0,0 +1,170 @@
+# from sys import argv
+# MPI INIT called here
+from mpi4py import MPI
+# from mpi_logic import fullSyncro, masterWorker
+class HPCGridSearch:
+    def __init__(self, param_grid, rank=None, size=None, comm=None, pschema="fs"):
+        self.params = param_grid
+        if comm is None:
+            self.comm = MPI.COMM_WORLD
+        if rank is None:
+            self.rank = self.comm.Get_rank()
+        if size is None:
+            self.size = self.comm.Get_size()
+        self.pschema = pschema
+    
+    def fit(self, x=None, y=None, augmentation=False, validation_split=0.0, validation_data=None, 
+            shuffle=True):
+        self.idata = x
+        self.odata = y
+        self.augmentation = augmentation
+        if pschema == 'fs':
+            self.fullSyncro()
+        elif pschema == 'mw':
+            self.masterWorker()
+        else:
+            self.rprint("ERROR: {} is not a valid option for a parallel schema!".format(self.pschema))
+    # Simple print commands to cut down on typing
+    def rprint(self, string, *args):
+        if self.rank == 0:
+            print("[{:4d}] {}".format(self.rank, string), flush=True)
+
+    def aprint(self, string, *args):
+        print("[{:4d}] {}".format(self.rank, string), flush=True)
+    ##
+
+    def fullSyncro(self):
+        # Determine additional input parameters
+        if self.augmentation:
+            self.rprint("Running augmented training now, with augmentation")
+        else:
+            self.rprint("Running training now, with no augmentation")
+        # Generate a full collection of combinations
+        allCombos = getMaxCombos(self.params)
+        cgen = lambda n: comboGenerator(n, params=self.params)
+        self.rprint("{} processes handling {} tasks".format(size, allCombos))
+        # Map over all things belonging to the process
+        allResults = []
+        with open('result-{:03d}.txt'.format(rank), 'w') as outfile:
+            # Clear out the old file
+            outfile.write("")
+            # pass
+
+        for i in range(rank, allCombos, size):
+                print("[{:3d}] Moving onto cgen({})".format(rank, i),  flush=True)
+                allResults.append(self.train_model(cgen(i), cv=KFolds)
+
+                with open('result-{:03d}.txt'.format(rank), 'a+') as outfile:
+                    outfile.write("{}\n".format(allResults[-1]))
+        return allResults
+
+
+    def masterWorker(self):
+        """
+
+        Sends out initial work orders.
+        Runs a for loop over additional work.
+        When ANY MPI process reports back as finished (confirmed via status)
+            That process is immediately send new work.
+        After all work has been sent out the master process waits for all
+            processes to finish.
+        The master process then collects the remaining work.
+
+        """
+        # Control the processes
+        if self.rank == 0:
+            self.rprint("Process {} ready to control!".format(rank))
+            cgen = lambda n: comboGenerator(n, params=self.params)
+            # First generate all work
+            # allCombos = generateAllCombinations(data)
+            allCombos = getMaxCombos(data)
+            self.rprint("{} processes handling {} tasks".format(size, allCombos))
+            # print("[{:3d}] {}".format(rank, allCombos))
+            # Do this with a generator later!
+
+            # Send work to all processes
+            for irank in range(1,size):
+                item = cgen(irank - 1)
+                comm.send(item, dest=irank, tag=0)
+                self.rprint("Work sent to process {}".format(irank))
+            totalWork = allCombos
+
+            for workNum in range(size - 1, totalWork):
+                # Now wait for completed work to be sent
+                status = MPI.Status()
+                res = comm.recv(source=MPI.ANY_SOURCE, tag=0,status=status)
+                irank = status.source
+                item = cgen(workNum)
+                self.rprint("Sending {} to process {}".format(item, irank))
+                self.comm.send(item, dest=irank, tag=0)
+                # Write work to file
+                with open('results.txt', 'w') as outfile:
+                    outfile.write("{}\n".format(res))
+            # Shut down!
+            # Collect remaining work
+            for irank in range(1,size):
+                # Collect all remaining work!
+                res = self.comm.recv(source=irank, tag=0, status=None)
+                with open('results.txt', 'w') as outfile:
+                    outfile.write("{}\n".format(res))
+                # Send shutdown signal
+                self.comm.send(None, dest=irank, tag=0)
+            # For book keeping sake, still tally completed work
+            self.rprint("Completed {} tasks!".format(totalWork))
+
+        # Do work!
+        else:
+            self.aprint("Process {} ready to work!".format(rank))
+            res = {}
+            master = 0
+            res = comm.recv(source=master, tag=0, status=None)
+            self.aprint("Received {} from Process {}".format(res, master))
+            while res is not None:
+                # Do job
+                res = self.train_model(res, augmentation=augmentation, cv=KFolds,
+                        root=root, rank=rank)
+                # Send Completed Work
+                comm.send(res, dest=master, tag=0)
+                # Receive Job 
+                res = comm.recv(source=master, tag=0, status=None)
+                self.aprint("Received {} from Process {}".format(res, master))
+            # Shut down!
+
+    def train_model(self):
+        pass
+     
+### THESE ARE HELPER FUNCTIONS
+def getMaxCombos(params):
+    return reduce(lambda x, y: x*y, (map(lambda key: len(params[key]),
+        params.keys())))
+
+def comboGenerator(n, params={}):
+    """(int, dictionary) -> dictionary
+
+    Acts as a pseudo-generator. Given an index it returns a dictionary.
+    If you were to generate a list of all possible combinations of keys
+    in the dictionary then this would return the ith item in that list.
+
+    While there is a lot more sorting, it should be very very fast especially
+    when compared the time and memory it takes to fully generate the whole list
+    of combos instead.
+    """
+    keys = sorted(params.keys(), reverse=True)
+    maxCombos = getMaxCombos(params)
+    # print(maxCombos)
+    if n >= maxCombos:
+        return None
+
+    if "data_multiplier" in keys:
+        del keys[keys.index("data_multiplier")]
+    else:
+        params["data_multiplier"] = [1]
+    # keys.insert(0, "data_multiplier")
+    keys.append("data_multiplier")
+    result = {}
+    for key in keys:
+        val  = len(params[key])
+        result[key] = [params[key][n % val]]
+        n = n // val
+    # return keys, totals
+    return result
