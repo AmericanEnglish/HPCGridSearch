2728e5618c2f1eb82f4cdddbca34e7eda3110f05
Carlos Barajas <barajasc@umbc.edu>
2020-03-18 15:00:48 -0400

    converted entirely to pandas for data arrangement and parsing

--- postProcess.py
+++ postProcess.py
@@ -1,29 +1,35 @@
 from re import split
 # from functools import filter
-from json import load as loadf
 from json import loads 
-
+from pandas import DataFrame
 from os import getcwd as pwd
 from datetime import datetime
+# Just use pandas!
+# https://www.geeksforgeeks.org/how-to-randomly-select-rows-from-pandas-dataframe/
 
-def makeUnique(someList):
-    return list(set(someList))
+# For extracting things
+def extract1(row):
+    return row.map(extract2)
+def extract2(val):
+    if isinstance(val, list) and len(val) == 1:
+        return val[0]
+    return val
 
-def createTable(data, jdata, categories=None, groupby = None, endocap=None):
-    keys = sorted(jdata.keys())
+def createTable(data, categories=None, groupby = None, endocap=None):
+    keys = sorted(data.columns.to_list())
     if categories is None:
         print("Pick Column/Row")
         # Assuming all entries have the same categories
-        keys = sorted(jdata.keys())
         for index, key in enumerate(keys):
             print("{}: {}".format( index, key))
         categories = input("")
         # print(categories)
         categories = split("[\\s\:\\+\\,]", categories)
         categories = list(map(lambda x: keys[int(x)], categories))
-
-    otherKeys = list(filter(lambda key: key != categories[0] and
-            key != categories[1], keys))# and
+    # Remove extra keys
+    otherKeys = keys[:]
+    del otherKeys[otherKeys.index(categories[0])]
+    del otherKeys[otherKeys.index(categories[1])]
 
     if groupby is None:
                 # key != groupby, keys)
@@ -34,199 +40,138 @@ def createTable(data, jdata, categories=None, groupby = None, endocap=None):
 
     print("Build table with: {}".format(categories))
     print("Grouped by {}".format(groupby))
-    otherKeys = list(filter(lambda key: key != groupby, otherKeys))
+    del otherKeys[otherKeys.index(groupby)]
+    del otherKeys[otherKeys.index('time')]
+    del otherKeys[otherKeys.index('acc')]
+    del otherKeys[otherKeys.index('tacc')]
     defaults = {}
     if len(otherKeys) != 0:
-        # Be sure that there are multiple values
         for key in otherKeys:
-            if len(jdata[key]) > 1:
+            # Get the unique values for the unused key
+            # So that we can generate a table based on just one of the values
+            u = data[key].unique()
+            # print(u)
+            u.sort()
+            if len(u) > 1:
                 print("Please choose default {} value by index for creation".format(key))
-                for index, val in enumerate(jdata[key]):
-                    print("{}: {}".format(index, jdata[key][index]))
-                defaults[key] = jdata[key][int(input("").strip())]
+                for index, val in enumerate(u):
+                    print("{}: {}".format(index, u[index]))
+                s = int(input("").strip())
+                print("Selected:{} -- {}".format(s, u[s]))
+                defaults[key] = u[int(s)]
+            elif len(u) == 1:
+                defaults[key] = u[0]
             else:
-                defaults[key] = jdata[key][0]
+                print("{} has no values!".format(u))
                 # print("defaults", defaults)
-    # Filter out all dicts which don't meet the defaults
-    # print(data)
-    usableDicts = data
-    for key in defaults.keys():
-        usableDicts = list(filter(lambda aDict: defaults[key] == aDict[key],
-                usableDicts))
-    # Table 1 is time and table 2 is accuracy
+
     alltables1 = []
     alltables2 = []
     table1 = "% {} -- {}\n%{}\n".format(datetime.now(), pwd(), defaults)
     table2 = "% {} -- {}\n%{}\n".format(datetime.now(), pwd(), defaults)
-    # Autogenerate the caption too
-    table1 += "\\caption{Wall time for " 
-    table1 += "{} versus {} grouped by {} with ".format(categories[0],
-            categories[1], groupby).lower().replace("_", " ")
-    table2 += "\\caption{Accuracy for " 
-    table2 += "{} versus {} grouped by {} with ".format(categories[0],
-            categories[1], groupby).lower().replace("_", " ")
+    # Redo everything using only the above variables!
+    # Sample table -- categories[0] == vX, categories[1] == dX, groupby
+    # determines subtable values
+    # [     ][v1][v2][v2]
+    # [   d1][f1][f2][f3]
+    # [   d2][f4][f5][f6]
+    # [   d3][f7][f8][f9]
+    sdata = data.copy()
+    for key in defaults.keys():
+        # Get rid of all the data which does not have the required defaults
+        sdata = sdata.loc[sdata[key] == defaults[key]]
+    # Generate the first pandas table using pivots for time
+    # print(sdata)
+    tabs_t = []
+    tabs_a = []
+    groupby_vals = sorted(sdata[groupby].unique())
+    for val in groupby_vals:
+        # print(sdata.loc[sdata[groupby] == val])
+        tabs_t.append(sdata.loc[sdata[groupby] == val].pivot(index=categories[1], 
+            columns=categories[0], values='time'))
+        tabs_a.append(sdata.loc[sdata[groupby] == val].pivot(index=categories[1], 
+            columns=categories[0], values='acc'))
+    # Generate all header and caption data for the table
+    categories = list(map(lambda x: x.replace("_", " "), categories))
+    groupby = groupby.replace("_", " ")
+    defs = list(map(lambda x: "{} {}".format(x, defaults[x]).replace("_", " "),
+        sorted(defaults.keys())))
+    # The hardest part is building the table header :(
+    width = len(tabs_t[0].columns.to_list()) 
+    table1 += "\\caption{" +"Timing for {} versus {} grouped by {} with {} {}.".format(
+            categories[0], categories[1], groupby, ", ".join(defs), endocap) \
+    + "}\n\\vspace{0.5\\baselineskip" + "}\n" + "\\begin{tabular" \
+    + "}{r"+ "{}".format("c"*width) + "}\\hline\n"
+    table2 += "\\caption{" +"Timing for {} versus {} grouped by {} with {} {}.".format(
+            categories[0], categories[1], groupby, ", ".join(defs), endocap) \
+    + "}\n\\vspace{0.5\\baselineskip" + "}\n" + "\\begin{tabular" \
+    + "}{r"+ "{}".format("c"*width) + "}\\hline\n"
+    def help1(v, t="time"):
+        v = v.split(",")
+        v[0] = "{:16}".format(v[0].strip())
+        for i, val in enumerate(v[1:]):
+            if t == "time":
+                if val:
+                    v[i+1] = " {:8}".format(v[i+1].split(".")[0])
+                else: # Empty string
+                    v[i+1] = "       N/A"
+            else:
+                if val:
+                    v[i+1] = "{:9.2f}".format(float(v[i+1])*100)
+                else: # Empty string
+                    v[i+1] = "    N/A"
+        return v
 
-    # if len(defaults.keys()) == 1:
-        # table1 += " with a fixed value for "
-        # table2 += " with a fixed value for "
-    # else:
-        # table1 += " with fixed values for "
-        # table2 += " with fixed values for "
-    # table1 += "".format(", ".join(sorted(defaults.keys())).lower())
-    # The fixed default portion might need to come out eventually
-    # This is also a special way to compute the generic string for
-    # a fixed value for {variable}, {variable}  of {value}, {value} respectively.
-    # table1 += "{}".format(", ".join(
-        # sorted(defaults.keys()))).lower().replace("_", " ")
-    # table2 += "{}".format(", ".join(
-        # sorted(defaults.keys()))).lower().replace("_", " ")
-    table1 +=  "{}".format(", ".join(
-        list(map(lambda x: "{} {}".format(x, defaults[x]),
-            sorted(defaults.keys()))))).replace("_", " ")
-    table2 +=  "{}".format(", ".join(
-        list(map(lambda x: "{} {}".format(x, defaults[x]),
-            sorted(defaults.keys()))))).replace("_", " ")
-
-    # table1 += "{}".format(", ".join(
-        # list(map(lambda x: str(defaults[x]), sorted(defaults.keys()))))) 
-    # table2 += "{}".format(", ".join(
-        # list(map(lambda x: str(defaults[x]), sorted(defaults.keys()))))) 
-    if eoc is None:
-        table1 += "."
-        table2 += "."
-    else:
-        table1 += " {}.".format(eoc)
-        table2 += " {}.".format(eoc)
-    # The end of the caption
-    table1 +="}\n\\vspace{0.5\\baselineskip}\n"
-    table2 +="}\n\\vspace{0.5\\baselineskip}\n"
-    # Include the tabular 
-    table1 += "\\begin{tabular}{r" 
-    table1 +=  "c" * (len(jdata[categories[1]]))
-    table1 +=  "} \\hline\n"
-    table2 += "\\begin{tabular}{" \
-            + "r{}".format("c"*(len(jdata[categories[1]]))) \
-            + "} \\hline\n"
-    # print("usableDicts", usableDicts)
-    for group in jdata[groupby]:
-        body1 = ""
-        body2 = ""
-        header = ""
-        # header += "\\multicolumn{"+ "{}".format(len(jdata[categories[1]]) + 1) \
-                # + "}{l}{" + "{} {}".format(group, groupby) + "}" + "\\\\\n"
-        header += "{} {}".format(group, groupby).title().replace("_"," ") \
-                + " & \\multicolumn{" \
-                + "{}".format(len(jdata[categories[1]]) )  \
-                + "}{c}{" + "{}".format(categories[1]).title().replace("_"," ") + "}\\\\\n"
-                # + "}{l}{" + "{}/{}".format(*categories).title().replace("_"," ") + "}\\\\\n"
-        # header += "\\hline\n"
-        header += "{:15} ".format(categories[0]).title().replace("_", " ")
-        header += "".join(list(map(lambda val: "& {:8}".format(val), 
-            map(lambda val: str(val), sorted(jdata[categories[1]])))))
-        header += "\\\\\n\\hline\n"
-        body1 += header
-        body2 += header
-        groupDicts = list(filter(lambda aDict: aDict[groupby] == group,
-            usableDicts))
-        # print("groupDicts", groupDicts)
-        for catval in jdata[categories[0]]:
-            row1, row2 = createRow(jdata, categories, catval, groupDicts)
-            body1 += row1
-            body2 += row2
-        # table1 += "\\hline\\hline\n"
-        alltables1.append(body1)
-        # table2 += "\\hline\\hline\n"
-        alltables2.append(body2)
-    table1 += "\\hline\\hline\n".join(alltables1) + "\\hline\n\\end{tabular}"
-    table2 += "\\hline\\hline\n".join(alltables2) + "\\hline\n\\end{tabular}"
+    for i in range(len(tabs_t)):
+        tab = tabs_t[i].to_csv().strip().split("\n")
+        # print(tab)
+        table1 += "{} {}".format(groupby_vals[i], groupby.title()) \
+        + " & \\multicolumn{" + "{}".format(width)  +"}{c}{" \
+        + "{}".format(categories[0]).title() + "}\\\\\n" 
+        subhead = tab[0].split(",")
+        subhead = "{:16} &".format(subhead[0]).title().replace("_", " ") \
+                + "&".join(list(map(lambda x: " {:8} ".format(x), subhead[1:])))
+        table1 += subhead + "\\\\\n\\hline\n"
+        table1 += " \\\\\n".join(list(map(
+            lambda row: " &".join(help1(row, "time")),
+            tab[1:])))
+        if i < len(tabs_t) - 1:
+            table1 += "\\\\ \n\\hline\\hline\n"
+        else:
+            table1 += " \\\\ \n\\hline"
+        # Accuracy Table
+        tab = tabs_a[i].to_csv().strip().split("\n")
+        # print(tab)
+        table2 += "{} {}".format(groupby_vals[i], groupby.title()) \
+        + " & \\multicolumn{" + "{}".format(width)  +"}{c}{" \
+        + "{}".format(categories[0]).title() + "}\\\\\n" 
+        subhead = tab[0].split(",")
+        subhead = "{:16} &".format(subhead[0]).title().replace("_", " ") \
+                + "&".join(list(map(lambda x: " {:8} ".format(x), subhead[1:])))
+        table2 += subhead + "\\\\\n\\hline\n"
+        table2 += " \\\\\n".join(list(map(
+            lambda row: " &".join(help1(row, "acc")),
+            tab[1:])))
+        if i < len(tabs_a) - 1:
+            table2 += " \\\\ \n\\hline\\hline\n"
+        else:
+            table2 += " \\\\\n\\hline"
+        # print(tabs_a[i].to_csv().strip().split("\n"))
+    # print(table2)
+    table1 += "\n\end{tabular}"
+    table2 += "\n\end{tabular}"
     return table1, table2
 
-def createRow(jdata, categories, catval, dicts):
-    # print(categories)
-    # print(catval)
-    # print("dicts", dicts)
-    times = {}
-    acc = {}
-    columns = list(filter(lambda entry: entry[categories[0]] == catval, dicts))
-    # print("columns",columns)
-    # columns = list(
-    for entry in columns:
-        times[entry[categories[1]]] = entry['time']
-        acc[entry[categories[1]]] = entry['acc']
-    # map(lambda entry:
-            # putInto(times, entry[categories[1]], entry['time']), columns)
-    # map(lambda entry:
-            # putInto(acc,   entry[categories[1]], entry['acc']),  columns)
-        # s[entry[categories[0]]] = entry['time'], columns)# )
-    row1 = "{:15} ".format(catval)
-    row2 = "{:15} ".format(catval)
-    # print(acc)
-    # print(times, acc)
-    for val in jdata[categories[1]]:
-        if val in times.keys() and bool(times[val]):
-            row1 += "& {:8}".format(times[val])
-            row2 += "& {:8}".format(acc[val])
-        else:
-            row1 += "& {:8}".format("N/A")
-            row2 += "& {:8}".format("N/A")
-
-    row1 += "\\\\\n"
-    row2 += "\\\\\n"
-    return row1, row2
-
-def putInto(aDict, key, val):
-    aDict[key] = val
-    return True
-
-def parseLine(line):
-    if "::" in line:
-        # entries = line.split("::")[::2]
-        entries = line.split("::")
-        entries = list(map(lambda entry: entry.strip(), entries))
-        # entries = [entries[0].strip(), entries[1].strip()]
-        # Cut off the "." in the time entry
-        # print(entries)
-        ptime = entries[2].split(".")[0]
-        acc = entries[1].split("->")[1].strip()
-        # Create a dictionary from the first entry
-        aDict = loads(entries[0].replace("'","\""))
-        aDict['time'] = ptime
-        aDict['acc'] = "{:6.2f}".format(float(acc)*100)
-        # print(aDict)
-    else:
-        # aDict = loads(entries[0].replace("'","\""))
-        line = line.replace("'",'"')
-        aDict = loads(line)
-        # Cut off fractional seconds
-        aDict['time'] = aDict['time'].split(".")[0]
-        # Converts accuracy to a percent
-        try:
-            aDict['acc'] = "{:6.2f}".format(float(aDict['acc'])*100)
-        except ValueError:
-            aDict['acc'] = str(aDict['acc']*100).split(".")[0]
-    return aDict
-
-    
-def loadData(filename, jfilename):
-    with open(jfilename, 'r') as infile:
-        jdata = infile.read().strip()
+def loadData(filename):
     with open(filename, 'r') as infile:
         data = infile.read().strip()
     if not data.strip():
         print("File is empty: {}".format(filename))
         exit()
-    elif not jdata.strip():
-        print("jFile is empty: {}".format(jfilename))
-        exit()
-    jdata = loads(jdata)
-    data = list(map(parseLine, data.split("\n")))
-    for aDict in data:
-        keys = aDict.keys()
-        for key in keys:
-            if key != 'time' and key != 'acc':
-                aDict[key] = aDict[key][0]
-    # print(data)
-    return data, jdata
+    data = list(map(lambda x: loads(x.replace("'", '"')), data.split("\n")))
+    data = DataFrame(data)
+    data = data.apply(extract1)
+    return data
 
 
 def saveOutput(ofilename, table):
@@ -250,24 +195,23 @@ if __name__ == "__main__":
         eoc = None
 
     # Take in the json file to avoid a ton of work in the table builder
-    try:
-        if "-f" in argv and "-j" in argv:
-            filename = argv[argv.index('-f') + 1]
-            jfilename = argv[argv.index('-j') + 1]
-            data, jdata = loadData(filename, jfilename)
-            table1, table2 = createTable(data, jdata, categories=categories,
-                    groupby=groupby, endocap=eoc)
-        else:
-            print("Not enough information provided!")
-            print("python script.py -f result.txt -j associatedJSON.json")
-            exit()
-        if '-o' in argv:
-            ofilename1 = argv[argv.index('-o') + 1]
-            ofilename2 = argv[argv.index('-o') + 2]
-            saveOutput(ofilename1, table1)
-            saveOutput(ofilename2, table2)
-        else:
-            saveOutput("./timings.tex", table1)
-            saveOutput("./accuracy.tex", table2)
-    except Exception as e:
-        print(e)
+    # try:
+    if "-f" in argv:
+        filename = argv[argv.index('-f') + 1]
+        data = loadData(filename)
+        table1, table2 = createTable(data, categories=categories,
+                groupby=groupby, endocap=eoc)
+    else:
+        print("Not enough information provided!")
+        print("python script.py -f result.txt -j associatedJSON.json")
+        exit()
+    if '-o' in argv:
+        ofilename1 = argv[argv.index('-o') + 1]
+        ofilename2 = argv[argv.index('-o') + 2]
+        saveOutput(ofilename1, table1)
+        saveOutput(ofilename2, table2)
+    else:
+        saveOutput("./timings.tex", table1)
+        saveOutput("./accuracy.tex", table2)
+    # except Exception as e:
+        # print(e)
